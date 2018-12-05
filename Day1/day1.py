c803181f00d478d7dfae2e07a9e2e7c797e0f7f1
LukeGall <44098963+LukeGall@users.noreply.github.com>
2018-12-05 20:14:32 +0000

    day1 part 2

--- Day1/day1.py
+++ Day1/day1.py
@@ -8,15 +8,38 @@ Created on Wed Dec  5 19:21:34 2018
 input_file = open("input","r")
 lines=input_file.read().split("\n")
 
-start=0
+cur=0
+twice=0
+listOfValues={}
 
-for line in lines:
-    if(len(line) != 0):
-        sign=line[0]
-        
-        if(sign=='+'):
-            start+=int(line[1:])
-        else:
-            start-=int(line[1:])
+while twice==0:
+    for line in lines:
+        if(len(line) != 0):
+            sign=line[0]
+            
+            if(sign=='+'):
+                cur+=int(line[1:])
+                
+                if(cur in listOfValues):
+                    val=int(listOfValues.get(cur))
+                    listOfValues.update({cur:val+1})
+                    
+                    if(listOfValues[cur] >=2):
+                        twice=cur
+                        break
+                else:
+                    listOfValues.update({cur:1})
+                
+            else:
+                cur-=int(line[1:])
+                if(cur in listOfValues):
+                    val=listOfValues.get(cur)
+                    listOfValues.update({cur:val+1})
+                    if(listOfValues[cur] >=2):
+                        twice=cur
+                        break
+                else:
+                    listOfValues.update({cur:1})
 
-print(start)
+
+print(twice)
