ece68ddd45c7ab2da104301b082a500443198bbe
Luke Gall <2298070G@student.gla.ac.uk>
2019-01-14 10:24:31 +0000

    Update day2.py

--- Day2/day2.py
+++ Day2/day2.py
@@ -5,26 +5,20 @@ Created on Wed Dec  5 20:34:49 2018
 @author: lukep
 """
 
-input_file=open("input","r")
-lines=input_file.read().split("\n")
+input_file = open("input", "r")
+lines = input_file.read().split("\n")
 lines.remove("")
 
 for x in range(len(lines)):
     for y in range(len(lines)):
-        if x>y:
-            word1=lines[x]
-            word2=lines[y]
-            if(word1!= word2):
-                pos=[]
+        if x > y:
+            word1 = lines[x]
+            word2 = lines[y]
+            if(word1 != word2):
+                pos = []
                 for i in range(len(word1)):
                     if(word1[i] != word2[i]):
-                            pos.append(i)
+                        pos.append(i)
                 if(len(pos) == 1):
-                    newStr=word1.replace(word1[pos[0]],"")
+                    newStr = word1.replace(word1[pos[0]], "")
                     print(newStr)
-                    
-                        
-        
-        
-        
-        
